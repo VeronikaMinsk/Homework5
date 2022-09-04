6fe92789ff84b36c26d700c241d51bf1a678cf64
Veronika <griva.verona@gmail.com>
2022-09-05 00:43:36 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+Array2(array);
+
+void Array2(int[] array)
+
+{
+    int size = array.Length;
+    int Summ = 0;
+   
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1,10);  
+            if(i % 2 != 0) 
+            Summ += array[i];
+    }
+    Console.WriteLine($"Массив из {array.Length} чисел:");
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+    Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях: {Summ}");
+}
